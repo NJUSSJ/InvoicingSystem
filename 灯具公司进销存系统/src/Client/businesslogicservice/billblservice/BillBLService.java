633713ce9fd987a6d6361c9f5a2e0f8c4fdee663
SSJ <njussj@gmail.com>
2017-10-19 22:52:37 +0800

    接口

--- 灯具公司进销存系统/src/Client/businesslogicservice/billblservice/BillBLService.java
+++ 灯具公司进销存系统/src/Client/businesslogicservice/billblservice/BillBLService.java
@@ -1,5 +1,19 @@
 package Client.businesslogicservice.billblservice;
 
-public interface BillBLService {
+import Client.vo.BillVO;
 
+public interface BillBLService {
+	public BillVO makeImportbill(BillVO bill);
+	public BillVO makeImpor_Returntbill(BillVO bill);
+	public BillVO makeSalebill(BillVO bill);
+	public BillVO makeSale_Returnbill(BillVO bill);
+	public BillVO makePaybill(BillVO bill);
+	public BillVO makeGetbill(BillVO bill);
+	public BillVO makeGiftbill(BillVO bill);
+	public BillVO makeOverbill(BillVO bill);
+	public BillVO makeDamagebill(BillVO bill);
+	public BillVO makeWaringbill(BillVO bill);
+	public BillVO makeCashbill(BillVO bill);
+	public String conveyBill(BillVO bill);
+	public String checkBill(BillVO bill);
 }
