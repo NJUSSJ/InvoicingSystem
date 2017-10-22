c9150942652a84d0a4fa7e92cf9f8653a9b6c392
xy-PC\xy <805781038@qq.com>
2017-10-22 20:11:56 +0800

    结束啦！吃饭去

---
+++ InvoicingSystemClient/src/driver/Client.java
@@ -0,0 +1,70 @@
+package driver;
+
+import java.rmi.RemoteException;
+
+import stubs.AccountBLService_Stub;
+import stubs.AccountDatabaseServiceMySqlImpl_Stub;
+import stubs.BillBLService_Stub;
+import stubs.BillDatabaseServiceMySqlImpl_Stub;
+import stubs.CommodityBLService_Stub;
+import stubs.CommodityDatabaseServiceMySqlImpl_Stub;
+import stubs.LogBLService_Stub;
+import stubs.LogDatabaseServiceMySqlImpl_Stub;
+import stubs.MemberBLService_Stub;
+import stubs.MemberDatabaseServiceMySqlImpl_Stub;
+import stubs.PromotionBLService_Stub;
+import stubs.PromotionDataServiceMySqlImpl_Stub;
+import stubs.SaleBLService_Stub;
+import stubs.SaleDatabaseServiceMySqlImpl_Stub;
+import stubs.TableBLService_Stub;
+import stubs.TableDatabaseServiceMySqlImpl_Stub;
+import stubs.UserBLService_Stub;
+import stubs.UserDatabaseServiceMySqlImpl_Stub;
+
+public class Client {
+public static void main(String[] args){
+	AccountBLService_Driver abldriver=new AccountBLService_Driver();
+	abldriver.drive(new AccountBLService_Stub(0, null, 0, null));
+	AccountDataService_Driver addriver=new AccountDataService_Driver();
+	addriver.drive(new AccountDatabaseServiceMySqlImpl_Stub());
+	BillBLService_Driver bbldriver=new BillBLService_Driver();
+	bbldriver.drive(new BillBLService_Stub(null, null, null));
+	BillDataService_Driver bddriver=new BillDataService_Driver();
+	bddriver.drive(new BillDatabaseServiceMySqlImpl_Stub());
+	CommodityBLService_Driver cbldriver=new CommodityBLService_Driver();
+	cbldriver.drive(new CommodityBLService_Stub(null, 0, 0, 0, 0, 0, null, null));
+	CommodityDataService_Driver cddriver=new CommodityDataService_Driver();
+	cddriver.drive(new CommodityDatabaseServiceMySqlImpl_Stub());
+	LogBLService_Driver lbldriver=new LogBLService_Driver();
+	lbldriver.drive(new LogBLService_Stub(null, null, null));
+	LogDataService_Driver lddriver=new LogDataService_Driver();
+	lddriver.drive(new LogDatabaseServiceMySqlImpl_Stub());
+	MemberBLService_Driver mbldriver=new MemberBLService_Driver();
+	mbldriver.drive(new MemberBLService_Stub(null, null, null, null, 0, null, 0, null, 0, 0));
+	MemberDataService_Driver mddriver=new MemberDataService_Driver();
+	try {
+		mddriver.drive(new MemberDatabaseServiceMySqlImpl_Stub());
+	} catch (RemoteException e) {
+		// TODO 自动生成的 catch 块
+		e.printStackTrace();
+	}
+	PromotionBLService_Driver pbldriver=new PromotionBLService_Driver();
+	pbldriver.drive(new PromotionBLService_Stub(null, null, 0, 0, null, 0, null, 0));
+	PromotionDataService_Driver pddriver=new PromotionDataService_Driver();
+	pddriver.drive(new PromotionDataServiceMySqlImpl_Stub());
+	UserBLService_Driver ubldriver=new UserBLService_Driver();
+	ubldriver.drive(new UserBLService_Stub());
+	UserDataService_Driver uddriver=new UserDataService_Driver();
+	uddriver.drive(new UserDatabaseServiceMySqlImpl_Stub());
+	SaleBLService_Driver sbldriver=new SaleBLService_Driver();
+	sbldriver.drive(new SaleBLService_Stub(null, null, 0, 0, null, 0, null, 0, null, null, null, 0, null, 0, null, 0, 0, null, 0, 0, 0, 0, 0, null));
+	SaleDataService_Driver sddriver=new SaleDataService_Driver();
+	sddriver.drive(new SaleDatabaseServiceMySqlImpl_Stub());
+	TableBLService_Driver tbldriver=new TableBLService_Driver();
+	tbldriver.drive(new TableBLService_Stub(null, null, 0, 0, 0, 0, 0, 0, 0, null, null, 0, null));
+	TableDataService_Driver tddriver=new TableDataService_Driver();
+	tddriver.drive(new TableDatabaseServiceMySqlImpl_Stub());
+
+
+}
+}
