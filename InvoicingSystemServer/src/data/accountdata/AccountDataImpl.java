30c6115d3d8347a71def3e29b6b0275551b5cd8d
SSJ <njussj@gmail.com>
2017-11-27 20:25:23 +0800

    casual

--- InvoicingSystemServer/src/data/accountdata/AccountDataImpl.java
+++ InvoicingSystemServer/src/data/accountdata/AccountDataImpl.java
@@ -1,5 +1,62 @@
 package data.accountdata;
+/**
+ * @author shisj
+ */
+import java.rmi.RemoteException;
 
-public class AccountDataImpl {
+import dataservice.accountdataservice.accountdataservice;
+import po.AccountPO;
+
+public class AccountDataImpl implements accountdataservice {
+
+	/**
+	 * 根据id查找账户
+	 */
+	@Override
+	public AccountPO find(long id) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	/**
+	 * 插入新的账户对象
+	 */
+	@Override
+	public void insert(AccountPO po) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	/**
+	 * 删除账户对象
+	 */
+	@Override
+	public void delete(AccountPO po) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 维护账户对象
+	 */
+	@Override
+	public void update(AccountPO po) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finish() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- InvoicingSystemServer/src/po/AccountPO.java
+++ InvoicingSystemServer/src/po/AccountPO.java
@@ -1,5 +1,36 @@
 package po;
 
-public class AccountPO {
+import java.io.Serializable;
 
+public class AccountPO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	long id;
+	String bank;
+	double deposit;
+	
+	public AccountPO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AccountPO(long id, String bank,double deposit) {
+		this.bank=bank;
+		this.deposit=deposit;
+		this.id=id;
+	}
+	
+	public double getDposit() {
+		return deposit;
+	}
+	
+	public String getBank() {
+		return bank;
+	}
+	
+	public long getID() {
+		return id;
+	}
 }
+
