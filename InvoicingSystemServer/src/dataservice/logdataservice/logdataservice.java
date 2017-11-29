60a46b56caeb3220b33aabbbfcb52fbdf749b7c1
SSJ <njussj@gmail.com>
2017-11-29 23:25:09 +0800

    log vo po

--- InvoicingSystemServer/src/dataservice/logdataservice/logdataservice.java
+++ InvoicingSystemServer/src/dataservice/logdataservice/logdataservice.java
@@ -7,6 +7,8 @@ import po.LogPO;
 public interface logdataservice {
 
 	public void insert(LogPO po) throws RemoteException;
+	public void findLogs() throws RemoteException;
+	public void findLogbyOperator(long operatorID) throws RemoteException;
 	public void delete(LogPO po) throws RemoteException;
 	public void update(LogPO po) throws RemoteException;
 	public void init() throws RemoteException;
