72637e71268a48af034ba793209b1965755b63e5
xy-PC\xy <805781038@qq.com>
2017-11-30 13:54:27 +0800

    log实现1.0

--- InvoicingSystemClient/src/businesslogic/logbl/LogController.java
+++ InvoicingSystemClient/src/businesslogic/logbl/LogController.java
@@ -1,13 +1,35 @@
 package businesslogic.logbl;
 
+import java.util.ArrayList;
+
+import businesslogicservice.logblservice.LogBLService;
 import vo.LogVO;
 
-public class LogController {
+public class LogController implements LogBLService{
 	Log logImpl=new Log();
-	public boolean addRecord(LogVO vo){
-		return logImpl.addRecord(vo);
+
+	@Override
+	public boolean addLog(LogVO vo) {
+		// TODO 自动生成的方法存根
+		return false;
 	}
-	public String checkRecord(){
+
+	@Override
+	public ArrayList<LogVO> findLog() {
+		// TODO 自动生成的方法存根
 		return null;
 	}
+
+	@Override
+	public ArrayList<LogVO> findLogByUserID(long userid) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public ArrayList<LogVO> findLogByType(String type) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+	
 }
--- InvoicingSystemClient/src/businesslogicservice/logblservice/LogBLService.java
+++ InvoicingSystemClient/src/businesslogicservice/logblservice/LogBLService.java
@@ -1,8 +1,12 @@
 package businesslogicservice.logblservice;
 
+import java.util.ArrayList;
+
 import vo.LogVO;
 
 public interface LogBLService {
-	public boolean addRecord(LogVO vo);
-	public void checkRecord();
+	public boolean addLog(LogVO vo);
+	public ArrayList<LogVO> findLog();
+	public ArrayList<LogVO> findLogByUserID(long userid);
+	public ArrayList<LogVO> findLogByType(String type);
 }
