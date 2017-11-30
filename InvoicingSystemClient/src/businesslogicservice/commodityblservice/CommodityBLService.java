35a777ae12c656afc3e2101aeea6844a4f36d346
xy-PC\xy <805781038@qq.com>
2017-11-30 19:32:49 +0800

    commodity实现1.0

--- InvoicingSystemClient/src/businesslogicservice/commodityblservice/CommodityBLService.java
+++ InvoicingSystemClient/src/businesslogicservice/commodityblservice/CommodityBLService.java
@@ -9,27 +9,6 @@ import vo.CommodityVO;
 
 
 public interface CommodityBLService {
-	
-	public CommodityVO toCommodityVO(String name,long id,String model, int stockNum, double importPrice, double salePrice,
-			double lateImportPrice, double lateSalePrice,String parent);
-
-	public CommodityVO getCommodity(String name);
-	
-	public ArrayList<CommodityVO> getCommodityList(String field);
-	
-	public CategoryVO getCategory(String name);
-	
-	public ArrayList<CategoryVO> getCategoryList(String field);
-	
-	public String addCommodity(CommodityVO c);
-	
-	public String addCategory(CategoryVO c);
-	
-	public String deleteCommodity(CommodityVO c);
-	
-	public String deleteCategory(CategoryVO c);
-	
-	public String modifyCommodity(CommodityVO c);
-	
-	public String modifyCategory(CategoryVO c);
+	public boolean addCommodity(CommodityVO commodityVO);
+	public boolean delete
 }
