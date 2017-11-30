2d02d97021d7e0832ee60a20d93c60d65845019f
161250076 <161250076@mail.smal.nju.edu.cn>
2017-11-30 14:42:03 +0800

    lsy

--- InvoicingSystemClient/src/MainApp/MainApp.java
+++ InvoicingSystemClient/src/MainApp/MainApp.java
@@ -9,6 +9,12 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import presentation.mainui.AdministerMainViewController;
+import presentation.mainui.FianceMainViewController;
+import presentation.mainui.LoginPanelController;
+import presentation.mainui.ManagerMainViewController;
+import presentation.mainui.SaleMainViewController;
+import presentation.mainui.StockMainViewController;
 import runner.ClientRunner;
 
 /**
@@ -58,11 +64,156 @@ public class MainApp extends Application {
 			AnchorPane loginPanel=loader.load();
 			
 			MainApp.rootlayout.setCenter(loginPanel);
+			
 		} catch (IOException e) {
 			// TODO: handle exception
 		}
 	}
 	
+	/*
+	 * 显示财务人员主界面
+	 */
+	public static  void showFianceMainUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/FianceMainUI.fxml"));
+			
+			AnchorPane fianceMainUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(fianceMainUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示管理人员主界面
+	 */
+	public static  void showAdministerMainUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/AdministMainUI.fxml"));
+			
+			AnchorPane administMainUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(administMainUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示销售人员主界面
+	 */
+	public static  void showSaleMainUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/SaleMainUI.fxml"));
+			
+			AnchorPane saleMainUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(saleMainUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示库存管理人员主界面
+	 */
+	public static  void showStockMainUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/StockMainUI.fxml"));
+			
+			AnchorPane stockMainUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(stockMainUI);
+		
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示总经理主界面
+	 */
+	public static  void showManagerMainUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/ManagerMainUI.fxml"));
+			
+			AnchorPane managerMainUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(managerMainUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示邮箱界面
+	 */
+	public static  void showEmailUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/mainui/EmailUI.fxml"));
+			
+			AnchorPane emailUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(emailUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示日记界面
+	 */
+	public static  void showLogUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/logui/LogUI.fxml"));
+			
+			AnchorPane logUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(logUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 显示用户管理界面
+	 */
+	public static  void showUserChangeUI() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/presentation/userui/UserChangeUI.fxml"));
+			
+			AnchorPane userChangeUI=loader.load();
+			
+			MainApp.rootlayout.setCenter(userChangeUI);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	/**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return MainApp.primarystage;
+    }
+ 
+    
 	public static void main(String[] args) {
 		new ClientRunner();
 		launch(args);
--- InvoicingSystemClient/src/presentation/mainui/AdministerMainViewController.java
+++ InvoicingSystemClient/src/presentation/mainui/AdministerMainViewController.java
@@ -1,5 +1,46 @@
 package presentation.mainui;
 
-public class AdministerMainViewController {
+import java.net 
 
+.URL;
+import java.util.ResourceBundle;
+
+import MainApp.MainApp;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class AdministerMainViewController implements Initializable {
+	@FXML
+	private Label id;
+	
+	@FXML
+	private ImageView email;
+	
+	@FXML
+	private ImageView log;
+	
+	@FXML
+	private ImageView userChange;
+	@FXML
+	private Label logout;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void showlogUI(){
+		MainApp.showLogUI();
+	}
+    public void showEmailUI(){
+		MainApp.showEmailUI();
+	}
+    public void showuserChangeUI(){
+	    MainApp.showUserChangeUI();
+    }
+    public void showLoginUI(){
+		MainApp.showLoginUI();
+   	}
 }
