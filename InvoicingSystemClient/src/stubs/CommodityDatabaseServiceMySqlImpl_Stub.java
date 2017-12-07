5622b22795ac263e5527f224f774d998378f1b2a
xy-PC\xy <805781038@qq.com>
2017-12-07 23:19:34 +0800

    消除报错

--- InvoicingSystemClient/src/stubs/CommodityDatabaseServiceMySqlImpl_Stub.java
+++ InvoicingSystemClient/src/stubs/CommodityDatabaseServiceMySqlImpl_Stub.java
@@ -56,14 +56,5 @@ public class CommodityDatabaseServiceMySqlImpl_Stub implements CommodityDataServ
 		return false;
 	}
 
-<<<<<<< HEAD
-	@Override
-	public ArrayList<CommodityPO> findCommoditiesbyParent(long parentid) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-=======
->>>>>>> ddec58460cd9051688106ac1485cb6e080a1ed55
 	
 }
