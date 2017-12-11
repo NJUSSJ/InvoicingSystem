7cc636a3477ba4d5b7884f65426dd069d09d7810
SSJ <njussj@gmail.com>
2017-12-11 11:13:40 +0800

    member data interface changed

--- InvoicingSystemClient/src/dataservice/memberdataservice/MemberDataService.java
+++ InvoicingSystemClient/src/dataservice/memberdataservice/MemberDataService.java
@@ -10,6 +10,7 @@ public interface MemberDataService extends Remote {
 
 	public MemberPO findMemberbyName(String name) throws RemoteException;
 	public ArrayList<MemberPO> findMembers() throws RemoteException;
+	public MemberPO findMemberbyID(long id) throws RemoteException;
 	public ArrayList<MemberPO> findMembersbyField(String field) throws RemoteException;
 	public boolean insert(MemberPO po) throws RemoteException;
 	public boolean delete(MemberPO po) throws RemoteException;
