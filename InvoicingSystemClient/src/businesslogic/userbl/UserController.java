b2c0d8fb34bbf5a03dd167e304acfafb8057fabc
xy-PC\xy <805781038@qq.com>
2017-12-24 18:31:36 +0800

    log完成

--- InvoicingSystemClient/src/businesslogic/userbl/UserController.java
+++ InvoicingSystemClient/src/businesslogic/userbl/UserController.java
@@ -11,43 +11,36 @@ public class UserController implements UserBLService {
 	User userImpl=new User();
 	@Override
 	public boolean addUser(UserVO a) {
-		// TODO Auto-generated method stub
 		return userImpl.addUser(a);
 	}
 
 	@Override
 	public boolean deleteUser(UserVO a) {
-		// TODO Auto-generated method stub
 		return userImpl.deleteUser(a);
 	}
 
 	@Override
 	public UserVO findUserbyName(String name) {
-		// TODO Auto-generated method stub
 		return userImpl.findUserbyName(name);
 	}
 
 	@Override
 	public boolean updateUser(UserVO a) {
-		// TODO Auto-generated method stub
 		return userImpl.updateUser(a);
 	}
 
 	@Override
 	public UserVO toUserVO(UserPO userPO) {
-		// TODO 自动生成的方法存根
 		return userImpl.toUserVO(userPO);
 	}
 
 	@Override
 	public UserVO findUserByID(long id) {
-		// TODO 自动生成的方法存根
 		return userImpl.findUserByID(id);
 	}
 
 	@Override
 	public ArrayList<UserVO> findUsers() {
-		// TODO 自动生成的方法存根
 		return userImpl.findUsers();
 	}
 
