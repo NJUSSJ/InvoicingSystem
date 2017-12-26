3e9783a4333285670186bbe1d57c1fb395dbef56
SSJ <njussj@gmail.com>
2017-12-26 12:12:35 +0800

    server

--- InvoicingSystemServer/src/rmi/RemoteHelper.java
+++ InvoicingSystemServer/src/rmi/RemoteHelper.java
@@ -1,7 +1,9 @@
 
 package rmi;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
@@ -24,12 +26,16 @@ public class RemoteHelper {
 	 */
 	public void initServer() {
 		DataRemoteObject dataRemoteObject;
-		
+		String localhost=null;
 		try {
 			dataRemoteObject=new DataRemoteObject();
-			
+			try {
+				localhost=InetAddress.getLocalHost().getHostAddress();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
 			LocateRegistry.createRegistry(8889);
-			String addressName="rmi://localhost:8889/BulbManagement";
+			String addressName="rmi://"+localhost+":8889/BulbManagement";
 			Naming.bind(addressName, dataRemoteObject);
 			
 			
